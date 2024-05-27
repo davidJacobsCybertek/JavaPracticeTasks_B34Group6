1c51431ab8c1bcc8625fb4d86aa0b1b25275420a
arlmcdaniel <arlmcdaniel@hotmail.com>
2024-05-27 16:12:15 -0500

    OddOrEven

--- src/Arlen/Week1/Task1.java
+++ src/Arlen/Week1/Task1.java
@@ -2,5 +2,19 @@ package Arlen.Week1;
 
 public class Task1 {
 
+    public static void main(String[] args) {
+
+
+        oddOrEven(50);
+    }
+
+    public static void oddOrEven(int n) {
+        if (n % 2 == 0) {
+            System.out.println("Num is Odd");
+        }else {
+            System.out.println("Num is Even");
+        }
+    }
+
     //question 1
 }
