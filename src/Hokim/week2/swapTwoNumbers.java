03257fb17b84dc1af16dd7724f2d91880bbedb11
Hokimhokens <hokimhokens@gmail.com>
2024-05-30 18:16:21 -0500

    my solution to tasks

---
+++ src/Hokim/week2/swapTwoNumbers.java
@@ -0,0 +1,22 @@
+package Hokim.week2;
+
+public class swapTwoNumbers {
+    public static void main(String[] args) {
+        //Swap two variable' values without using a third variable
+        int a = 5;
+        int b = 7;
+        int temp;
+
+//      temp = a;
+//      a = b;
+//      b = temp;
+
+        a = b+a; //12;
+        b = a-b; //12-7 = 5;
+        a = a-b; //12-5 = 7;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+    }
+}
--- src/Week2/swapTwoNumbers.java
+++ src/Week2/swapTwoNumbers.java
@@ -4,4 +4,5 @@ public class swapTwoNumbers {
     public static void main(String[] args) {
         //Swap two variable' values without using a third variable
 
+    }
 }
