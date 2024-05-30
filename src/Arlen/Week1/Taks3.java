0ae641e9c46574908992027383e37ebb8ae11563
arlmcdaniel <arlmcdaniel@hotmail.com>
2024-05-30 13:17:17 -0500

    completed the rest of the tasks

--- src/Arlen/Week1/Taks3.java
+++ src/Arlen/Week1/Taks3.java
@@ -1,5 +1,30 @@
 package Arlen.Week1;
 
 public class Taks3 {
-    //question3
+
+    public static void main(String[] args) {
+
+        finra();
+
+
+    }
+
+    public static void finra(){
+
+        for (int num = 1; num <= 30; num++) {
+
+            if (num % 3 == 0 && num % 5 == 0) {
+                System.out.println("FINRA");
+            } else if (num % 3 == 0) {
+                System.out.println("FIN");
+            } else if (num % 5 == 0) {
+                System.out.println("RA");
+            }else {
+
+            }
+
+        }
+
+    }
+
 }
--- src/Arlen/Week1/Task2.java
+++ src/Arlen/Week1/Task2.java
@@ -1,4 +1,34 @@
 package Arlen.Week1;
 
 public class Task2 {
-}
+
+    public static void main(String[] args) {
+
+      division(100,10);
+
+    }
+
+    public static void division(int num1, int num2) {
+        int count = 0;
+        if (num1 >= 0 && num2 >= 0) {
+            while (num1 >= num2){
+                num1 = num1 - num2;
+                count += 1;
+            }
+
+            System.out.println(count);
+
+        }else if (num1 >= 0 && num2 < 0 || num1 < 0 && num2 >= 0) {
+            while (Math.abs(num1) >= Math.abs(num2)){
+                num1 = Math.abs(num1) - Math.abs(num2);
+                count += 1;
+            }
+
+            System.out.println("-" + count);
+
+        }
+
+        }
+    }
+
+
