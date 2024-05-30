ec203993ec885e01c49e0aa67bd124b420a73e5c
DavidCydeo <david@cydeo.com>
2024-05-30 21:37:21 +0300

    Week 2 added

---
+++ src/Hokim/week2/oddOrEven.java
@@ -0,0 +1,9 @@
+package Hokim.week2;
+
+public class oddOrEven {
+    public static void main(String[] args) {
+        //Write  a method which can identifies given number is even or odd
+
+    }
+
+ }
