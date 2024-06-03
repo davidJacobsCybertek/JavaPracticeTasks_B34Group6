462508d48985f830d44212169c7f2ea8e907af0e
VEYSELBIGEC <veyselbgc@gmail.com>
2024-06-03 16:51:59 -0400

    Week1 solutions by Veysel for test!

---
+++ src/Veysel/Week1/divisionWithoutOperator.java
@@ -0,0 +1,14 @@
+package Veysel.Week1;
+
+public class divisionWithoutOperator {
+    public static void main(String[] args) {
+        //Write a method that can divide two numbers without using division operator
+
+
+
+    }
+
+// Week 1 solutions - test
+
+
+}
