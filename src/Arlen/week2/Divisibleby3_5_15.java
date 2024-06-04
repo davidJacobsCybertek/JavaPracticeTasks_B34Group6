f6949798662eb1085881308361ce87e4acf52363
arlmcdaniel <arlmcdaniel@hotmail.com>
2024-06-04 17:33:10 -0500

    week2 task1 complete!

---
+++ src/Arlen/week2/Divisibleby3_5_15.java
@@ -0,0 +1,28 @@
+package Arlen.week2;
+
+public class Divisibleby3_5_15 {
+
+    public static void main(String[] args) {
+        // Numbers - Swap Numbers
+
+        String divideBy3 = "";
+        String divideBy5 = "";
+        String divideBy15 = "";
+
+        for (int i = 1; i <= 100; i++){
+            if (i % 3 == 0){
+                divideBy3 = divideBy3 + i;
+            }else if (i % 5 == 0){
+                divideBy5 = divideBy5 + i;
+            }else if (i % 15 == 0){
+                divideBy15 = divideBy15 + i;
+            }
+        }
+
+        System.out.println(divideBy3);
+        System.out.println(divideBy5);
+        System.out.println(divideBy15);
+
+    }
+
+}
