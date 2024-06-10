9295585d74ba2a548eaf73eaa2ddc9d20a88cd9e
DavidCydeo <david@cydeo.com>
2024-06-10 21:58:29 +0300

    Week 3 added

---
+++ src/Week3/reverseNegativeNumber.java
@@ -0,0 +1,9 @@
+package Week3;
+
+public class reverseNegativeNumber {
+    //Write a return method that can reverse digits of a negative number and return it as int
+    //input: -35  output: -53
+
+
+}
+
