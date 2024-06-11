1010ddee935308a13534ef48a8f61ca3f701bf3d
DavidCydeo <david@cydeo.com>
2024-06-11 14:23:35 +0300

    Week 1.1 solution added

--- src/Week1/divisionWithoutOperator.java
+++ src/Week1/divisionWithoutOperator.java
@@ -5,8 +5,29 @@ public class divisionWithoutOperator {
         //Write a method that can divide two numbers without using division operator
 
 
+        System.out.println(divide(13,0));
 
 
 
+    }
+
+    private static int divide(int divident, int divider) {
+
+        if(divider==0){
+            System.out.print("Math ERROR");
+            return Integer.MIN_VALUE;
+        }
+
+        int division=0;
+
+        while (divident>=divider){
+
+            divident-=divider;
+            division++;
+
+        }
+
+
+        return division;
     }
 }
