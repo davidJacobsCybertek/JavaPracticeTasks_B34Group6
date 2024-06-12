5daebcbb39b961e60e7502f768554d8dc15efb72
nazik <nazik.kbv@gmail.com>
2024-06-11 19:20:35 -0500

    last try

--- src/Nazik/week1/divisionWithoutOperator.java
+++ src/Nazik/week1/divisionWithoutOperator.java
@@ -4,6 +4,7 @@ public class divisionWithoutOperator {
     public static void main(String[] args) {
         //Write a method that can divide two numbers without using division operator
 // nazik answered 1st task
+        // Last try
 
 
     }
