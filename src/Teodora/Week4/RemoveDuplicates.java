759db2cc2bae72375576dd54bb5aebd55c014af9
teodoranaunova <naunova123@hotmail.com>
2024-06-12 19:04:09 -0500

    Updated all the files

---
+++ src/Teodora/Week4/RemoveDuplicates.java
@@ -0,0 +1,7 @@
+package Teodora.Week4;
+
+public class RemoveDuplicates {
+    /*Write a return method that can remove the duplicated values from String
+        Ex:  removeDup("AAABBBCCC")  ==> ABC*/
+
+}
